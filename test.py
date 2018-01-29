869369740181b32169a96de0d24a933da66a1756
danielpicone <35905640+danielpicone@users.noreply.github.com>
2018-01-29 15:27:51 +1100

    initial commit on this branch

---
+++ test.py
@@ -0,0 +1,11 @@
+# This is a simple python script to test python and github
+
+import numpy as np
+
+random_numbers = np.random.rand(10)
+# FInd the mean
+print("The mean is " + str(np.mean(random_numbers)))
+# Find the median
+print("The median is "+ str(p.median(random_numbers)))
+
+# This is another change
---
+++ test2.py
@@ -0,0 +1,13 @@
+# This is another simple python script to test python and github
+
+import numpy as np
+
+random_numbers = np.random.rand(10)
+# FInd the mean
+print("The mean is " + str(np.mean(random_numbers)))
+# Find the median
+print("The median is "+ str(p.median(random_numbers)))
+
+# This is another change
+
+# I want to change it in the current branch only
